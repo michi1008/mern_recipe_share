7b396e9f78d3c220d9469fddf7816c4417e91f37
michiko lange taguchi <51973693+michi1008@users.noreply.github.com>
2024-04-21 17:20:40 -0700

    fix build

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
+  const __dirname = path.resolve();
   app.get("/", (req, res) => {
     res.send("API is running....");
   });
